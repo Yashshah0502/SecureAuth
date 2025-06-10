ad641bf27aa1ec3bab3ca9215532f22b1b4b57d1
Yashshah0502 <113469955+Yashshah0502@users.noreply.github.com>
2025-06-10 15:23:15 -0700

    We added the pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Profile from "./Pages/Profile";
+import SignIn from "./Pages/SignIn";
+import Signup from "./Pages/Signup";
+import About from "./Pages/About";
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/Profile" element={<Profile />}></Route>
+        <Route path="/SignIn" element={<SignIn />}></Route>
+        <Route path="/Signup" element={<Signup />}></Route>
+        <Route path="/About" element={<About />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
