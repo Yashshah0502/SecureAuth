da638134a62e341a830538a329b41562764ce9d8
Yashshah0502 <113469955+Yashshah0502@users.noreply.github.com>
2025-10-21 12:16:45 -0400

    Implement signin functionality with JWT authentication; update routes and add jsonwebtoken dependency

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/errors.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -8,6 +10,27 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json({ message: "User Created sucessfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) next(errorHandler(404, "User not found"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) next(errorHandler(401, "Wrong password"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const expiresIn = 24 * 60 * 60 * 1000; // 24 hours
+    const { password: hashedpassword, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      }, { maxAge: expiresIn })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
